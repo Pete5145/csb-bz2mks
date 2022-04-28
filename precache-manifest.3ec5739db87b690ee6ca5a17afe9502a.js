7453a37a53a86f83fb0f1c988212a07461b98ff7
Code Sandbox <hello@codesandbox.io>
2022-04-28 12:40:19 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.3ec5739db87b690ee6ca5a17afe9502a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ca5bd28b8281e441fe8ef9368f422c",
+    "url": "https://pete5145.github.io/csb-bz2mks/index.html"
+  },
+  {
+    "revision": "0b834a920de7d1609468",
+    "url": "https://pete5145.github.io/csb-bz2mks/static/js/2.2d8dda31.chunk.js"
+  },
+  {
+    "revision": "ddd7a40f4c94bafd5758",
+    "url": "https://pete5145.github.io/csb-bz2mks/static/js/main.67ab66da.chunk.js"
+  },
+  {
+    "revision": "8ed803946d32ac3c2cfb",
+    "url": "https://pete5145.github.io/csb-bz2mks/static/js/runtime-main.c5c757fc.js"
+  }
+]);
